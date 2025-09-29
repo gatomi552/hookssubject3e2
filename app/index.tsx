8d7f578e00e699f64908f0d85cd00fc154d74d7d
matiasmaldonado526 <matiasmaldonado526@gmail.com>
2025-09-29 19:51:20 +0000

    ✨feat(index) creacion y refaccion de un contador -creando un contador xd

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,29 @@
-import { Text, View } from "react-native";
+import { Text, View, Pressable } from "react-native";
+import { useState } from "react";
 
 export default function Index() {
+  const [contador, setcontador] = useState(0);
+
+
   return (
     <View
       style={{
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
-      }}
-    >
+      }}>
+
       <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Pressable onPress={(aumentador) => setcontador(contador + 1)} onLongPress={() => setcontador(contador -1)}>
+        <Text>contador: {contador}</Text>
+      </Pressable>
     </View>
   );
+
+  function aumentador() {
+    setcontador(contador + 1);
+  }
+  function reset() {
+    setcontador(contador - 1);
+  }
 }
